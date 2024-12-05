fcbcb0f8b05528c8bad551a93e99bf58c868ab25
Thomas Barrows <monkeydoodle@gmail.com>
2024-12-05 11:44:10 -0600

    added flask

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Hello World</title>
-</head>
-<body>
-    <h1>Hello World!</h1>
-</body>
-</html>
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')  # Render the HTML file
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)  # Make it accessible externally
+
